a0f38eed33d50eb25cb48f32d3a885afbcfdf3cf
Adinath_Pawar <166931227+Adinath-S-Pawar@users.noreply.github.com>
2026-01-25 11:10:20 +0530

    Add  TypeScript compilation and examples
     explaining TypeScript behavior and example code.

--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -5,6 +5,21 @@ let a: number = 12
 let b: string = '6'
 let c: number = 2
 
+/*
+let a = 12;
+let b = '6';
+console.log(a/b) -> 2 , valid javascript conversion
+*/
+
+/*
+in tsconfig.json
+"noEmitOnError" : true,
+will not create .js if compile errors in .ts
+cmd : tsc --noEmitOnError
+*/
 console.log(a / b)
 
 console.log(c * b)
+
+//watch for changes
+//tsc main.ts -w 
