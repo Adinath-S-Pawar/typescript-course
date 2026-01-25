882b23a8d04e5896b20aa39ca35a4f0bf4058d40
Adinath_Pawar <166931227+Adinath-S-Pawar@users.noreply.github.com>
2026-01-25 11:43:23 +0530

    Update sum function and add Symbol examples
    Refactor sum function to accept string type and add Symbol usage examples.

--- lesson02/src/main.ts
+++ lesson02/src/main.ts
@@ -9,10 +9,32 @@ isLoading = true
 album = 5150
 
 const sum = (a: number, b: string) => {
-    return a + b
+    return a + b    //string
 }
 
-let postId: string | number
+let postId: string | number    //union type
 let isActive: number | boolean
 
 let re: RegExp = /\w+/g
+//Without n, the value would be treated as a regular number type.
+const bigNumber: bigint = 9007199254740991n;
+
+const id1 = Symbol('id');
+const id2 = Symbol('id');
+//No two symbols are ever equal, even if they have the same description.
+console.log(id1 === id2); // false
+
+const userId = Symbol('userId');
+
+//Using Symbol as an object key
+const user = {
+  name: "Adinath",
+  [userId]: 101
+};
+console.log(user[userId]); // 101
+
+//Global Symbols
+const s1 = Symbol.for("shared");
+const s2 = Symbol.for("shared");
+console.log(s1 === s2); // true
+
