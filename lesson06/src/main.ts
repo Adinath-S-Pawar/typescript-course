436a470ffa335d3e6b7921731d0e87a8c0182885
Adinath_Pawar <166931227+Adinath-S-Pawar@users.noreply.github.com>
2026-01-27 16:58:46 +0530

    fix data assignment errors
     adjusted code to prevent errors.

--- lesson06/src/main.ts
+++ lesson06/src/main.ts
@@ -1,6 +1,6 @@
 class Coder {
 
-    secondLang!: string
+    secondLang!: string //will assign this later It tells TypeScript not to complain that the property isn’t set in the constructor
 
     constructor(
         public readonly name: string,
@@ -21,7 +21,7 @@ class Coder {
 
 const Dave = new Coder('Dave', 'Rock', 42)
 console.log(Dave.getAge())
-// console.log(Dave.age)
+// console.log(Dave.age) // legal js -> visible in console
 // console.log(Dave.lang)
 
 class WebDev extends Coder {
@@ -101,12 +101,13 @@ class Bands {
     constructor() {
         this.dataState = []
     }
-
+//getter
     public get data(): string[] {
         return this.dataState
     }
-
+//setters cannot return value
     public set data(value: string[]) {
+        //Array is the global JavaScript Array constructor
         if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
             this.dataState = value
             return
@@ -119,4 +120,4 @@ MyBands.data = ['Neil Young', 'Led Zep']
 console.log(MyBands.data)
 MyBands.data = [...MyBands.data, 'ZZ Top']
 console.log(MyBands.data)
-MyBands.data = ['Van Halen', 5150] // must be string data
+//MyBands.data = ['Van Halen', 5150] // must be string data
